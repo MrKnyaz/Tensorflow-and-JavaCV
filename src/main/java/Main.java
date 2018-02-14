4f41a7b5b51bf52ccbcb5c0dc6ca45017fe6404a
MrKnyaz <alexkn15@gmail.com>
2018-02-14 14:15:57 +0600

    commentary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,11 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        /* ImageProcessor returns 4-dimensional float array -> float[1][224][224][3]
+         * First dimension of the array is a batch for neural network,
+         * for this simple example I use just one image at a time for classification
+         * The code can be easily modified to load and classify multiple images.
+         * */
         ImageProcessor imageProcessor = new ImageProcessor();
         Classificator classificator = new Classificator();
         System.out.println(classificator.classify(imageProcessor.loadAndNormalizeImage("images/hyndai.jpg")));
